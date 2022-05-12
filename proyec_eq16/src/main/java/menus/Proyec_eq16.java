bd845a247cf74b32fba6519b2d501bf81ee19973
Monserivas25 <79218801+Monserivas25@users.noreply.github.com>
2022-05-11 20:46:22 -0500

    Add files via upload

---
+++ proyec_eq16/src/main/java/menus/Proyec_eq16.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package menus;
+
+/**
+ *
+ * @author monse
+ */
+public class Proyec_eq16 {
+
+    public static void main(String[] args) {
+        portada obj =new portada ();
+            obj.setVisible(true);
+            }
+    }
+
